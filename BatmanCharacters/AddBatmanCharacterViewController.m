137b11591319f1b92ac0dee4cb073242d525f4f4
Punit Lad <plad@thoughtworks.com>
2015-02-24 15:14:18 -0500

    Punit - added view controllers for table and add

---
+++ BatmanCharacters/AddBatmanCharacterViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddBatmanCharacterViewController.m
+//  BatmanCharacters
+//
+//  Created by plad on 2015-02-24.
+//  Copyright (c) 2015 plad. All rights reserved.
+//
+
+#import "AddBatmanCharacterViewController.h"
+
+@interface AddBatmanCharacterViewController ()
+
+@end
+
+@implementation AddBatmanCharacterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ BatmanCharacters/BatmanCharactersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BatmanCharactersTableViewController.h
+//  BatmanCharacters
+//
+//  Created by plad on 2015-02-24.
+//  Copyright (c) 2015 plad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BatmanCharactersTableViewController : UITableViewController
+
+@end
